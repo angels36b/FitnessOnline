7f5f2ad004aa1e9b3144f64a5d203b523ed7294a
angel david <angel.36b@gmail.com>
2026-09-13 16:00:35 +0300

    feat: Creation of CRUD To WebSite

---
+++ FrontEnd/src/app/services/video.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VideoService {
+  private apiUrl = 'http://localhost:3000/video';
+
+  constructor(private http: HttpClient) {}
+
+  getVideos(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
+
